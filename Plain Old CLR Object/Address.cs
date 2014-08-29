d2df3131411833083ffbb98428e99773ea6a7967
Igor Morais <mor41s.1gor@gmail.com>
2014-08-29 20:35:20 -0300

    Add "Plain Old CLR Object" example

---
+++ Plain Old CLR Object/Address.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Copyright (c) 2014 HollowSoft @IgorMorais
+ *
+ * Licensed under the Apache License, Version 2.0 (the “License”);
+ * you may not use this file except in compliance with the License.
+ *
+ *          You may obtain a copy of the License at
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an “AS IS” BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+using System;
+using TinyType.State;
+
+namespace TinyType
+{
+    /// <summary>
+    /// Igor Morais
+    /// mor41s.1gor@gmail.com
+    /// </summary>
+    public class Address
+    {
+        public long Id { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string Complement { get; set; }
+        public string ReferencePoint { get; set; }
+        public string City { get; set; }
+        public State State { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
